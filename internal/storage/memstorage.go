e2e88ee122e7683e2900bfefb30dfe6eb0ad5121
Oleg Kaschuk <OKaschuk@alfabank.ru>
2022-11-30 12:25:42 +0300

    feature(): add concurrent memStorage

--- internal/storage/memstorage.go
+++ internal/storage/memstorage.go
@@ -3,30 +3,59 @@ package storage
 import (
 	"fmt"
 	"github.com/c0dered273/go-adv-metrics/internal/metric"
+	"sync"
 )
 
 var (
-	str = make(map[string]metric.Metric)
+	storage = MemStorage{str: make(map[string]metric.Metric)}
 )
 
 type MemStorage struct {
+	mx  sync.RWMutex
+	str map[string]metric.Metric
+}
+
+func (m *MemStorage) get(key string) (metric.Metric, bool) {
+	m.mx.RLock()
+	defer m.mx.RUnlock()
+	v, ok := m.str[key]
+	return v, ok
+}
+
+func (m *MemStorage) put(key string, value metric.Metric) {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+	m.str[key] = value
+}
+
+func (m *MemStorage) iterateValues() <-chan metric.Metric {
+	c := make(chan metric.Metric)
+	go func() {
+		m.mx.RLock()
+		defer m.mx.RUnlock()
+		for _, v := range m.str {
+			c <- v
+		}
+		close(c)
+	}()
+	return c
 }
 
 func (m *MemStorage) Save(newMetric metric.Metric) error {
-	str[getID(newMetric)] = newMetric
+	m.put(getID(newMetric), newMetric)
 	return nil
 }
 
 func (m *MemStorage) FindAll() (metrics []metric.Metric, err error) {
 	var result []metric.Metric
-	for _, v := range str {
+	for v := range m.iterateValues() {
 		result = append(result, v)
 	}
 	return result, nil
 }
 
 func (m *MemStorage) FindByID(newMetric metric.Metric) (metric metric.Metric, err error) {
-	if result, ok := str[getID(newMetric)]; ok {
+	if result, ok := m.get(getID(newMetric)); ok {
 		return result, nil
 	}
 	return metric, fmt.Errorf("storage: not found: %v %v", newMetric.GetName(), newMetric.GetType())
@@ -37,5 +66,5 @@ func getID(newMetric metric.Metric) string {
 }
 
 func GetMemStorage() *MemStorage {
-	return &MemStorage{}
+	return &storage
 }
