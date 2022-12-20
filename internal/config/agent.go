67a2bee1721ada2a711af9c12d2af5d1505a00d2
Oleg Kaschuk <OKaschuk@alfabank.ru>
2022-12-20 11:46:43 +0300

    feature(): tests fix

--- internal/config/agent.go
+++ internal/config/agent.go
@@ -3,16 +3,9 @@ package config
 import (
 	"time"
 
-	"github.com/c0dered273/go-adv-metrics/internal/log"
-	"github.com/caarlos0/env/v6"
 	"github.com/spf13/pflag"
 )
 
-type AgentEnv struct {
-	Address        string        `env:"ADDRESS" envDefault:"http://localhost:8080"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
-}
 type AgentConfig struct {
 	Address        string
 	ReportInterval time.Duration
@@ -20,20 +13,15 @@ type AgentConfig struct {
 }
 
 func GetAgentConfig() *AgentConfig {
-	agentCfg := &AgentConfig{}
-	agentEnv := &AgentEnv{}
-	if err := env.Parse(agentEnv); err != nil {
-		log.Error.Fatal(err)
-	}
-
-	pflag.StringVarP(&agentCfg.Address, "address", "a", agentEnv.Address, "Server address:port")
-	pflag.DurationVarP(&agentCfg.ReportInterval, "report_interval", "r", agentEnv.ReportInterval, "Send metrics to server interval")
-	pflag.DurationVarP(&agentCfg.PollInterval, "poll_interval", "p", agentEnv.PollInterval, "Collect metrics interval")
+	agentFlag := AgentConfig{}
+	pflag.StringVarP(&agentFlag.Address, "address", "a", Address, "Server address:port")
+	pflag.DurationVarP(&agentFlag.ReportInterval, "report_interval", "r", ReportInterval, "Send metrics to server interval")
+	pflag.DurationVarP(&agentFlag.PollInterval, "poll_interval", "p", PollInterval, "Collect metrics interval")
 	pflag.Parse()
 
 	return &AgentConfig{
-		Address:        agentCfg.Address,
-		ReportInterval: agentCfg.ReportInterval,
-		PollInterval:   agentCfg.PollInterval,
+		Address:        lookupEnvOrString("ADDRESS", agentFlag.Address),
+		ReportInterval: lookupEnvOrDuration("REPORT_INTERVAL", agentFlag.ReportInterval),
+		PollInterval:   lookupEnvOrDuration("POLL_INTERVAL", agentFlag.PollInterval),
 	}
 }
--- internal/config/server.go
+++ internal/config/server.go
@@ -3,18 +3,9 @@ package config
 import (
 	"time"
 
-	"github.com/c0dered273/go-adv-metrics/internal/log"
 	"github.com/c0dered273/go-adv-metrics/internal/storage"
-	"github.com/caarlos0/env/v6"
 	"github.com/spf13/pflag"
 )
-
-type ServerEnv struct {
-	Address       string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
-	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	Restore       bool          `env:"RESTORE" envDefault:"true"`
-}
 
 type ServerConfig struct {
 	Address       string
@@ -25,22 +16,17 @@ type ServerConfig struct {
 }
 
 func GetServerConfig() *ServerConfig {
-	srvCfg := &ServerConfig{}
-	srvEnv := &ServerEnv{}
-	if err := env.Parse(srvEnv); err != nil {
-		log.Error.Fatal(err)
-	}
-
-	pflag.StringVarP(&srvCfg.Address, "address", "a", srvEnv.Address, "Server address:port")
-	pflag.DurationVarP(&srvCfg.StoreInterval, "store_interval", "i", srvEnv.StoreInterval, "Writing metrics to disk interval")
-	pflag.StringVarP(&srvCfg.StoreFile, "filename", "f", srvEnv.StoreFile, "Storage filename")
-	pflag.BoolVarP(&srvCfg.Restore, "restore", "r", srvEnv.Restore, "Is restore metrics from disk")
+	srvFlag := ServerConfig{}
+	pflag.StringVarP(&srvFlag.Address, "address", "a", Address, "Server address:port")
+	pflag.DurationVarP(&srvFlag.StoreInterval, "store_interval", "i", StoreInterval, "Writing metrics to disk interval")
+	pflag.StringVarP(&srvFlag.StoreFile, "filename", "f", StoreFile, "Storage filename")
+	pflag.BoolVarP(&srvFlag.Restore, "restore", "r", Restore, "Is restore metrics from disk")
 	pflag.Parse()
 
 	return &ServerConfig{
-		Address:       srvCfg.Address,
-		StoreInterval: srvCfg.StoreInterval,
-		StoreFile:     srvCfg.StoreFile,
-		Restore:       srvCfg.Restore,
+		Address:       lookupEnvOrString("ADDRESS", srvFlag.Address),
+		StoreInterval: lookupEnvOrDuration("STORE_INTERVAL", srvFlag.StoreInterval),
+		StoreFile:     lookupEnvOrString("STORE_FILE", srvFlag.StoreFile),
+		Restore:       lookupEnvOrBool("RESTORE", srvFlag.Restore),
 	}
 }
