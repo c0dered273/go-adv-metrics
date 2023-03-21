17c56d3c2537dc999b9ebba14a50903f3b3aa322
Oleg Kaschuk <OKaschuk@alfabank.ru>
2023-03-21 19:03:23 +0300

    feat(): add docs

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -53,6 +53,18 @@ import (
 	"honnef.co/go/tools/staticcheck"
 )
 
+// Выполняет проверку исходного кода комплектом статических анализаторов
+// Использование:
+//
+//	staticlint [-flag] [package]
+//	Запустить staticlint help для вывода помощи
+//
+// В комплект входят следующие анализаторы:
+// * все анализаторы из пакета golang.org/x/tools/go/analysis
+// * staticcheck - содержит более 150 различных проверок
+// * enumcase - проверяет каждый switch на использование всех const значений типа
+// * sqlrows - ищет ошибки при использовании sql.Rows
+// * osExit - ищет вызов os.Exit() в функции main()
 func main() {
 	osExit := &analysis.Analyzer{
 		Name: "osExit",
